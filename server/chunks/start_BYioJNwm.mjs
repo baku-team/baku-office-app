7561738bb30aacea4476ac8c49994e318e57d98e
baku-office-ci <ci@baku-office>
2026-06-28 15:45:27 +0000

    release: 6d2080f (auto-published from baku-office@main)

---
+++ server/chunks/start_BYioJNwm.mjs
@@ -0,0 +1,7 @@
+globalThis.process ??= {};
+globalThis.process.env ??= {};
+import { _ as _page } from "./start_B8leAday.mjs";
+const page = () => _page;
+export {
+  page
+};
---
+++ server/entry.mjs
@@ -0,0 +1,7 @@
+globalThis.process ??= {};
+globalThis.process.env ??= {};
+import { w } from "./chunks/worker-entry_D24No6WS.mjs";
+import "cloudflare:workers";
+export {
+  w as default
+};
